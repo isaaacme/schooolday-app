16cd18332cdc746c48019ee85b7d9a7dcf8f7b65
isaaacme <holla@isaaac.me>
2020-12-02 10:53:44 +0200

    fix logo issue

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Link from "next/link";
+import logo from "../assets/schooldaylogo.svg";
 import LoginSignInButtons from "./LoginSignInButtons";
 import Styles from "../styles/Nav.module.css";
 const Nav = () => {
   return (
     <nav className={Styles.Nav}>
-      <img src="{logo}" alt="" />
+      <Link href="/">
+        <a className={Styles.Logo}>
+          <img src={logo} alt="" />
+        </a>
+      </Link>
       <LoginSignInButtons />
     </nav>
   );
